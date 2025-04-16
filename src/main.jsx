0763e5b6cf91ec7a210ddf93304424ec6035a348
Irina Holler <irinaniko@hotmail.com>
2025-04-16 23:29:08 +0200

    Build and deploy-ready portfolio update

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles/global.scss'
-import './styles/index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles/global.scss';
+import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
